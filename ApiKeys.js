27fffb0acef7aefffee52bb8a9015912eff961ee
Ana <anaiv9cs@yahoo.co.uk>
2021-03-14 16:10:21 +0200

    firebase added

---
+++ ApiKeys.js
@@ -0,0 +1,11 @@
+export default {
+    FirebaseConfig: {
+        apiKey: "AIzaSyAHCAOSOlmfAbEjaEQKeNfYtHLEx-eUbio",
+        authDomain: "visittimisoara-76452.firebaseapp.com",
+        projectId: "visittimisoara-76452",
+        storageBucket: "visittimisoara-76452.appspot.com",
+        messagingSenderId: "643145208342",
+        appId: "1:643145208342:web:3a9069ab9cdd7857449b37",
+        measurementId: "G-QV4JCZ33J1"
+    },
+}
--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+import ApiKeys from './ApiKeys';
 
-export default function App() {
-  return (
+export default class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoadingComplete: false,
+    };
+    if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
+  }
+  render(){
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
       <StatusBar style="auto" />
     </View>
   );
+  }
+  
 }
 
 const styles = StyleSheet.create({
