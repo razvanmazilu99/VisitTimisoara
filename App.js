e6a817373b1b973c03bcc326dbb50b33cac93b33
Razvan <mazilu.razvan.moisil@gmail.com>
2021-03-15 14:44:57 +0200

    Welcome Page Design

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Image, View, Button, TouchableOpacity } from 'react-native';
 import * as firebase from 'firebase';
 import ApiKeys from './ApiKeys';
 
@@ -13,10 +13,32 @@ export default class App extends React.Component {
     };
     if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
   }
+
   render(){
+
     return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <Text style = {styles.textStyle}>
+        Welcome to Timișoara!</Text>
+      <Image 
+        style = {{  position: 'absolute',
+                    width: 465,
+                    height: 421,
+                    left: 71,
+                    top: 140,
+                    borderRadius: 170
+                }}
+        source = {require("./assets/U10.jpg")}
+      />
+
+      <TouchableOpacity onPress={console.log("Bau")} style={styles.buttonContainer}>
+            <Text style={{textAlign: 'center', fontFamily: 'Roboto', fontSize: 28, top: 0}}>Log in</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={console.log("Mau")} style={ [styles.buttonContainer, {top: 265, backgroundColor: 'white'}]}>
+            <Text style={{textAlign: 'center', fontFamily: 'Roboto', fontSize: 28, top: 0}}>Create account</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -25,10 +47,32 @@ export default class App extends React.Component {
 }
 
 const styles = StyleSheet.create({
+
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1A1B29',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  buttonContainer: {
+     top: 250,
+     height: 50,
+     width: 300,
+     justifyContent: 'center',
+     backgroundColor: '#59B298',
+     borderRadius: 25,
+  },
+
+  textStyle: {
+    position: 'absolute',
+    width: 414,
+    height: 179,
+    left: 0,      
+    top: 60,
+
+    color: 'white',
+    fontSize: 32,
+    textAlign: 'center',
   },
 });
