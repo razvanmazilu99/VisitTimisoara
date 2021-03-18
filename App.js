5bb8b587b772fc46fddebfbab9f3543b3ef8bdec
Ana <anaiv9cs@yahoo.co.uk>
2021-03-18 16:54:55 +0200

    Login Frontend

--- App.js
+++ App.js
@@ -3,76 +3,37 @@ import React from 'react';
 import { StyleSheet, Text, Image, View, Button, TouchableOpacity } from 'react-native';
 import * as firebase from 'firebase';
 import ApiKeys from './ApiKeys';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import WelcomeScreen from './screens/WelcomeScreen';
+import { not } from 'react-native-reanimated';
+import LoginScreen from './screens/LoginScreen';
+import SignupScreen from './screens/SignupScreen';
 
-export default class App extends React.Component {
+const Stack = createStackNavigator();
+export default function App()  {
   
-  constructor(props) {
+  /*constructor(props) {
     super(props);
     this.state = {
       isLoadingComplete: false,
     };
     if (!firebase.apps.length) { firebase.initializeApp(ApiKeys.FirebaseConfig); }
-  }
+  }*/
 
-  render(){
+ // render(){
 
     return (
-    <View style={styles.container}>
-      <Text style = {styles.textStyle}>
-        Welcome to Timișoara!</Text>
-      <Image 
-        style = {{  position: 'absolute',
-                    width: 465,
-                    height: 421,
-                    left: 71,
-                    top: 140,
-                    borderRadius: 170
-                }}
-        source = {require("./assets/U10.jpg")}
-      />
-
-      <TouchableOpacity onPress={console.log("Bau")} style={styles.buttonContainer}>
-            <Text style={{textAlign: 'center', fontSize: 28, top: 0}}>Log in</Text>
-      </TouchableOpacity>
-
-      <TouchableOpacity onPress={console.log("Mau")} style={ [styles.buttonContainer, {top: 265, backgroundColor: 'white'}]}>
-            <Text style={{textAlign: 'center', fontSize: 28, top: 0}}>Create account</Text>
-      </TouchableOpacity>
-
-      <StatusBar style="auto" />
-    </View>
-  );
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="WelcomeScreen" >
+          <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} />
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
   
-}
+//}
 
-const styles = StyleSheet.create({
 
-  container: {
-    flex: 1,
-    backgroundColor: '#1A1B29',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-
-  buttonContainer: {
-     top: 250,
-     height: 50,
-     width: 300,
-     justifyContent: 'center',
-     backgroundColor: '#59B298',
-     borderRadius: 25,
-  },
-
-  textStyle: {
-    position: 'absolute',
-    width: 414,
-    height: 179,
-    left: 0,      
-    top: 60,
-
-    color: 'white',
-    fontSize: 32,
-    textAlign: 'center',
-  },
-});
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,64 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, Image, View, Button, TouchableOpacity } from 'react-native';
+
+export default function WelcomeScreen({ navigation }){
+    return (
+        <View style={styles.container}>
+          <Text style = {styles.textStyle}>
+            Welcome to Timișoara!</Text>
+          <Image 
+            style = {{  position: 'absolute',
+                        width: 465,
+                        height: 421,
+                        left: 71,
+                        top: 130,
+                        borderRadius: 170
+                    }}
+            source = {require("../assets/U10.jpg")}
+          />
+    
+          <TouchableOpacity onPress={() => navigation.navigate('Login')} style={styles.buttonContainer}>
+                <Text style={{textAlign: 'center', fontSize: 28, top: 0}}>Log in</Text>
+          </TouchableOpacity>
+    
+          <TouchableOpacity onPress={console.log("Mau")} style={ [styles.buttonContainer, {top: 265, backgroundColor: 'white'}]}>
+                <Text style={{textAlign: 'center', fontSize: 28, top: 0}}>Create account</Text>
+          </TouchableOpacity>
+    
+          <StatusBar style="auto" />
+        </View>
+        
+      );
+}
+      
+const styles = StyleSheet.create({
+    
+    container: {
+        flex: 1,
+        backgroundColor: '#1A1B29',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    
+    buttonContainer: {
+         top: 250,
+         height: 50,
+         width: 300,
+         justifyContent: 'center',
+         backgroundColor: '#59B298',
+         borderRadius: 25,
+    },
+    
+    textStyle: {
+        position: 'absolute',
+        width: 414,
+        height: 179,
+        left: -14,      
+        top: 60,
+    
+        color: 'white',
+        fontSize: 32,
+        textAlign: 'center',
+    },
+});
