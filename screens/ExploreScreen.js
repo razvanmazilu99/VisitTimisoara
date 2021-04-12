ee3fddc1e6ca505ffabc8c0cbc1e78c57a6b8ac2
Ana <anaiv9cs@yahoo.co.uk>
2021-04-12 15:27:47 +0300

    Logout screen and logic added

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { Text, View, Image, ScrollView, Button } from 'react-native';
+import { Text, View, Image, Pressable, ScrollView, Button, TouchableOpacity } from 'react-native';
 import Attraction from '../attractions/Attraction';
 import AttractionList from './AttractionList';
 import AttractionSection from './AttractionSection';
+
+
 
 //var images = ["../assets/explore.png", "../assets/heart.png"];
 /*var images = ["https://images.app.goo.gl/LCFsVHiUWCaVWD8y8"];
@@ -33,26 +35,18 @@ function display(){
     });
 }*/
 
-//const firebase = require("firebase");
-//require("firebase/firestore");
-//var db = firebase.firestore();
-
 export default class ExploreScreen extends React.Component{
 
-    /*fire() {
-        db.collection("attractions").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data().name}`);
-            });
-        });
-    }*/
-
-    render(){
-          return(
+    render(){  
+        return(
             <View style={{flex: 1, backgroundColor: '#1A1B29'}}>
                 <View style={{flexDirection: 'row',  top: 17, height: 80}}>
-                    <Image style = {{ height: 30, width: 30, margin: 10, marginTop: 20 }} source = {require("../assets/user1.png")}/>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Profile')} style={{height: 30, width: 30, margin: 10, marginTop: 20}}>
+                        <Image style = {{ height: 30, width: 30}} source = {require("../assets/user1.png")}/>
+                    </TouchableOpacity>
+                    
                     <Text style={{ color: 'white', fontSize: 30, top: 10, margin: 7, fontWeight: 'bold' }}>Explore</Text>
+
                 </View>
                 <ScrollView>
                     <AttractionList/>
